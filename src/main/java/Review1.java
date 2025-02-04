cc2eee3aaea18bb731775721ba1358d3898a6771
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-02-04 19:48:43 +0000

    First Update

--- src/main/java/Review1.java
+++ src/main/java/Review1.java
@@ -1,23 +1,28 @@
-public class Review1 {
+public static void main {
+	public class Review1 {
 
-	public static void q1() {
-		//Write question 1 code here
+		public static void q1() {
+			System.out.println("There once was a man from St. Ives");
+		}
+
+		public static void q2() {
+			int firstNum = 9;
+			int secondNum = 5;
+			System.out.println(firstNum / secondNum);
+			System.out.println(firstNum % secondNum);
+		}
+
+		public static void q3() {
+			//Write question 3 code here
+		}
+
+		public static void q4() {
+			//Write question 4 code here
+		}
+
+		public static void q5() {
+			//Write question 5 code here
+		}
+
 	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
 }
